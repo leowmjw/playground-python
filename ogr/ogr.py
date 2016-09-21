5c46d94e5bc3fad4344fe95ce89b706945512ce1
Michael Leow <leow@sinarproject.org>
2016-09-21 14:25:27 +0800

    Summary: Streamline example for states; start with Perak
    Changesets:
    - Catch unexpected Empty Index; helped caught EC data problem
    - Tested on Perak data; works
    - Now can feature branch for rest of states as it comes out

--- ogr/ogr.py
+++ ogr/ogr.py
@@ -19,29 +19,55 @@ print("Sinar Project OGR Attribute Tables Processing!!!")
 # pprint.pprint(wip2.schema)
 # exit()
 
-a = CurrentShapefile()
+# a = CurrentShapefile()
 # Need to extract thigns out
-a.extract_feature_map()
+# a.extract_feature_map()
 # To duble check the mappings have been made
 # a.pprint_new_map()
 
 # after we extract out the csv??
-ec = ECRecommendation()
+# ec = ECRecommendation()
+# ec.extractdata()
+# ec.find_dm_match(a)
+# a.pprint_new_feature_map()
+
+# Write out the new output ..
+# msf = ModifiedShapefile(a.shapefile_source.driver, a.shapefile_schema)
+# Uncomment below to write to actual shapefile ..
+# msf.writeshapefile(a.get_new_feature_map())
+# For review case of Sarawak to check specific case
+# my_new_feature_map = a.get_new_feature_map()
+# for k in my_new_feature_map:
+#    if my_new_feature_map[k]['properties']['NAMA_DM'] == "BANGKIT":
+#        print(">>>>>>>>>>>>>>>>>>>>>>>>>")
+#        pprint.pprint(my_new_feature_map[k]['properties'])
+
+# print("<<<<<<<<<<    >>>>>>>>>")
+# csf = CurrentShapefile("./source/Kedah/02_Kdh_13_Ori.shp")
+# fmap = csf.extract_feature_map()
+
+
+# State: Perak
+# Important PATHS
+# CurrentShapefile: ./source/Perak/06_Prk_13_Ori.shp
+# ECRecommendation: ./source/Perak/Perak.csv
+# ModifiedShapefile: ./results/Perak/06-Perak-New-DM
+
+
+a = CurrentShapefile('./source/Perak/06_Prk_13_Ori.shp')
+# Need to extract thigns out
+a.extract_feature_map()
+# To double check the mappings have been made
+# a.pprint_new_map()
+
+# after we extract out the csv??
+ec = ECRecommendation('./source/Perak/Perak.csv')
 ec.extractdata()
 ec.find_dm_match(a)
 # a.pprint_new_feature_map()
 
 # Write out the new output ..
-msf = ModifiedShapefile(a.shapefile_source.driver, a.shapefile_schema)
+msf = ModifiedShapefile(a.shapefile_source.driver, a.shapefile_schema, './results/Perak/06-Perak-New-DM')
 # Uncomment below to write to actual shapefile ..
 msf.writeshapefile(a.get_new_feature_map())
-# For review case of Sarawak to check specific case
-my_new_feature_map = a.get_new_feature_map()
-for k in my_new_feature_map:
-    if my_new_feature_map[k]['properties']['NAMA_DM'] == "BANGKIT":
-        print(">>>>>>>>>>>>>>>>>>>>>>>>>")
-        pprint.pprint(my_new_feature_map[k]['properties'])
 
-# print("<<<<<<<<<<    >>>>>>>>>")
-# csf = CurrentShapefile("./source/Kedah/02_Kdh_13_Ori.shp")
-# fmap = csf.extract_feature_map()
